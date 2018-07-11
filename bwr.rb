8b9343eae156d8e3088976cfbebe00ef41ed248e
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-07-10 23:19:17 -0500

    Nuevos comandos agregados.

--- bwr.rb
+++ bwr.rb
@@ -9,18 +9,40 @@ class Main
 				system("cls")
 			elsif cmd[0..3] == "echo" && cmd.length > 5
 				puts(cmd[4..cmd.length - 1])
+			elsif cmd[0..4] == "touch" && cmd.length > 6
+				self.touch(cmd[6..cmd.length - 1])
+			elsif cmd[0..1] == "ls"
+				if cmd.length > 2
+					self.ls(cmd[3..cmd.length - 1])
+				else
+					self.ls()
+				end
 			elsif cmd == "exit"
 				break
 			else
-				puts("El comando #{cmd} es reconocido por el sistema")
+				puts("El comando #{cmd} es reconocido por el sistema.")
 			end
+		end
+	end
+	def ls(dir="")
+		Dir.glob("#{dir}\*").each do |d|
+			puts(d)
 		end
 	end
 	def cd(dir)
 		begin
 			Dir.chdir(dir)
 		rescue
-			puts("El sistema no ha encontrado el directorio #{dir}")
+			puts("El sistema no ha encontrado el directorio #{dir}.")
+		end
+	end
+	def touch(file)
+		begin
+			f = File.new(file, "w")
+			f.print("")
+			f.close
+		rescue
+			puts("Ha habido un problema al ejecutar el comando.")
 		end
 	end
 end
